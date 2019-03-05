73e17b08198990657130b131bcaa1461cf75287b
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-05 11:49:46 +0200

    basic slider working

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let slickOptions = {
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: $('pagination'),
+    dots: true,
+
+};
+
+$('.slider').slick(slickOptions);
+$(document).ready(function(){console.log('OK')});
+
+
+
