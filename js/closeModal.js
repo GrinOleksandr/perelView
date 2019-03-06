948b239fc34aeeb1ce03f561e5d629bcbb21ee0b
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-06 12:28:44 +0200

    close modal function JS created and it works now =)

--- js/closeModal.js
+++ js/closeModal.js
@@ -1,8 +1,7 @@
 let closeBtn = document.getElementsByClassName("close-modal-image")[0];
-closeBtn.add('click', close);
+closeBtn.addEventListener('click', close);
 
 function close (){
     let modal = document.getElementsByClassName('modal')[0];
     modal.style.display = 'none';
-    console.log('closed');
 }
