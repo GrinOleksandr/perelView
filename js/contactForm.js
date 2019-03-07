54f3156051b20e505410c0c5dd9b4d2e3445d774
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-07 12:10:02 +0200

    contact form basic inputs handling - OK

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,11 +1,15 @@
 let form = document.getElementById('contact-form');
 form.addEventListener('submit', submit);
 
-let submitBtn = document.getElementsByClassName('send-details')[0];
-submitBtn.addEventListener('click', submit);
+// let submitBtn = document.getElementsByClassName('send-details')[0];
+// submitBtn.addEventListener('click', submit);
 
 function submit(ev){
+    console.log('submitted');
     ev.preventDefault();
-    let data = new FormData(form);
-    console.log(data.fullName);
+    // let data = new FormData(form);
+
+    console.log( document.forms['contactForm'].elements['fullName'].value);
 }
+
+// document.forms['form'].elements['fullName'].value
