840abcf2809ee9a759c6de4f76a11c45de55ee29
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-07 11:54:06 +0200

    contact form input fields handling

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,1 +1,11 @@
 let form = document.getElementById('contact-form');
+form.addEventListener('submit', submit);
+
+let submitBtn = document.getElementsByClassName('send-details')[0];
+submitBtn.addEventListener('click', submit);
+
+function submit(ev){
+    ev.preventDefault();
+    let data = new FormData(form);
+    console.log(data.fullName);
+}
