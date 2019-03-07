fe0f6baadb3f36f0f942bada2dc81f4ad6f087b0
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-07 13:39:17 +0200

    contact form fullname validation allmost done again

--- js/contactForm.js
+++ js/contactForm.js
@@ -4,31 +4,31 @@ Form.addEventListener('submit', submit);
 const FullNameInputField = document.getElementsByClassName("full-name")[0];
 FullNameInputField.addEventListener('keyup', validateFullName);
 
-let agreementCheckbox = document.getElementsByClassName("agreement")[0];
+const AgreementCheckbox = document.getElementsByClassName("agreement")[0];
 
-// let submitBtn = document.getElementsByClassName('send-details')[0];
-// submitBtn.addEventListener('click', submit);
 
 function submit(ev){
-    console.log('submitted');
     ev.preventDefault();
     console.log("***********FORM SUBMITTED***********");
     console.log(`FULL NAME: ${Form.elements['fullName'].value}`);
     console.log(`Email: ${Form.elements['email'].value}`);
     console.log(`Phone: ${Form.elements['phone'].value}`);
     console.log(`APARTMENT TYPE: ${Form.elements['apartmentType'].value}`);
-    console.log(`SMS AGREEMENT: ${agreementCheckbox.checked}`);
+    console.log(`SMS AGREEMENT: ${AgreementCheckbox.checked}`);
     console.log("************************************");
 
 }
 
 function validateFullName(){
-    console.log("***FullNAME VALIDATING");
     let fullName = FullNameInputField.value;
     let sample = new RegExp("^[a-zA-Z\u0590-\u05fe '\"-]+$");
-    if(fullName.match(sample)){
-        console.log("OK")
+    let alert = document.getElementsByClassName("alert-full-name")[0];
+    if(fullName && fullName.match(sample)){
+       alert.style.display = "none";
+       console.log(fullName);
+       return true;
     }
-
-    else console.log("NOT OK");
+    else alert.style.display = "flex";
+    console.log(fullName);
+    return false;
 }
