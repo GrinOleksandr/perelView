7fe5016bfd946caa557c7653dc2b0043c531c8bb
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-12 16:01:52 +0200

    expiration block html structure added

--- js/elipsis.js
+++ js/elipsis.js
@@ -1,47 +1,38 @@
 const Comments = document.getElementsByClassName("comment-text");
 
-
 truncateAll(Comments);
 
-
-
-function truncateMe(target){
+function truncateMe(target) {
     let text = target.innerText;
-   if(text.length > 120){
+    if (text.length > 120) {
+        let shortText = text.substr(0, 120);
+        target.innerText = shortText;
+        target.parentElement.parentElement.style.setProperty("--display", "inline");
+        let ellipsis = target.parentElement.parentElement.getElementsByClassName("ellipsis")[0];
+        ellipsis.style.display = "flex";
+        ellipsis.classList.add("closed");
 
-       console.log(target.parentElement.parentElement.getElementsByClassName("ellipsis")[0]);
-       let shortText = text.substr(0,120);
-       target.innerText = shortText;
-       target.parentElement.parentElement.style.setProperty("--display", "inline");
-       let ellipsis = target.parentElement.parentElement.getElementsByClassName("ellipsis")[0];
-       ellipsis.style.display = "flex";
-       ellipsis.classList.add("closed");
-
-       ellipsis.addEventListener("click", function(ev){
-           return toggleText(ev.target,text, shortText);
-       });
-
-   }
+        ellipsis.addEventListener("click", function (ev) {
+            return toggleText(ev.target, text, shortText);
+        });
+    }
+}
+function truncateAll(targets) {
+    for (let i = 0; i < targets.length; i++) {
+        truncateMe(targets[i]);
+    }
 }
 
 function toggleText(target, string, shortString) {
-    console.log(target);
     let myTextBlock = target.parentElement.getElementsByClassName("comment-text")[0];
-    if(target.classList.contains("closed")){
+    if (target.classList.contains("closed")) {
         target.classList.remove("closed");
         target.parentElement.style.setProperty("--display", "none");
         myTextBlock.innerText = string;
-    }
-
-    else {
+    } else {
         myTextBlock.innerText = shortString;
         target.classList.add("closed");
         target.parentElement.style.setProperty("--display", "inline");
     }
 }
 
-function truncateAll(targets){
-    for (let i =0; i<targets.length; i++){
-        truncateMe(targets[i]);
-    }
-}
