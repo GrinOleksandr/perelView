11368fcfab72309799b0e4c32cc08e759747137f
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-18 16:35:57 +0200

    UTM handler added

--- js/utmHandler.js
+++ js/utmHandler.js
@@ -1,10 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", handleUTMSource);
+
+const UTMsList = {
+    google: '055-111 2222',
+    facebook: '055-111 3333',
+};
+
+function handleUTMSource() {
     let parsedUrl = new URL(window.location.href);
-    console.log(parsedUrl.searchParams.get("utm_source"));
-});
-
-
-
+    let utmSource = parsedUrl.searchParams.get("utm_source");
+    let newPhoneNumberString = `tel: ${UTMsList[utmSource] || '055-111 4444'}`;
+    document.getElementById("call").querySelector("a").href = newPhoneNumberString;
+}
 
 
 
