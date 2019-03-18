0331d76af3353e1ca4234b2b6a8625bea0c63fc4
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-18 16:11:55 +0200

    trying to parse url

---
+++ js/utmHandler.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let parsedUrl = new URL(window.location.href);
+    console.log(parsedUrl.searchParams.get("utm_source"));
+});
+
+
+
+//  http://localhost:63342/perelView/index.html?utm_source=Chicago%2BTribune&utm_medium=Newspaper%2BOctober&utm_campaign=Chicago%2BPPC%2BSale
