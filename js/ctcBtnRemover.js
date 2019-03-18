ca7e654d31c30b0b6778b8a1e405c4c863425bf7
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-18 17:20:19 +0200

    time check for CTCbutton added

---
+++ js/ctcBtnRemover.js
@@ -0,0 +1,8 @@
+// ********************Handles the "Click to call" button click********************
+const CallBtn = document.getElementById("call");
+CallBtn.addEventListener("click", checkTime);
+
+function checkTime() {
+
+    })
+}
