103fd6bd48ec3c18d05eb48eb2970b695e0c6113
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-19 10:33:38 +0200

    time check is now working

--- js/ctcBtnRemover.js
+++ js/ctcBtnRemover.js
@@ -1,8 +1,22 @@
-// ********************Handles the "Click to call" button click********************
-const CallBtn = document.getElementById("call");
-CallBtn.addEventListener("click", checkTime);
+// ********************Handles the time and if needed hides CTC button********************
+document.addEventListener("DOMContentLoaded", checkTime);
 
 function checkTime() {
 
+    fetch('http://perelview.herokuapp.com/time', {
+        method: 'GET',
+        headers:{
+            'Access-Control-Allow-Origin': "*"
+        }
     })
+        .then((response) =>
+            response.json()
+        ).then((data) =>{
+            console.log("OKK!K!K");
+        console.log(data);
+    })
+        .catch((err) => {
+            console.log(err);
+        })
+
 }
