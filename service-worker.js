06bea8b1773b2feaf711ac87adc6985e7a687a76
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-21 10:25:53 +0200

    trying webworker to work

--- service-worker.js
+++ service-worker.js
@@ -115,10 +115,15 @@ self.addEventListener('fetch', function(e) {
         );
     } else {
         e.respondWith(
-            caches.match(e.request).then(function(response) {
-                console.log('[ServiceWorker] Fetch Only', e.request.url);
-                return response || fetch(e.request);
+            caches.match(e.request).then(function (r) {
+                return r || fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                });
             })
         );
     }
 });
+
